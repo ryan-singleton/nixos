57e4425eef73a05986278aa7f493a184146d41de
ryan <ryan@northofreal.com>
2023-08-31 18:35:08 -0700

    updated

--- configuration.nix
+++ configuration.nix
@@ -10,193 +10,9 @@
       ./hardware-configuration.nix
     ];
 
-  # ====================
-  # User
-  # ====================
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ryan = {
-    isNormalUser = true;
-    description = "ryan";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-	    unzip
-    	git
-    	dotnet-sdk
-	    go
-	    rustup
-    	brave
-    	firefox
-    	kate
-    	neovim
-    	vscodium
-	    obs-studio
-    	krita
-    	discord
-    	spotify
-    	spectacle
-    ];
-  };
-
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "ryan";
-
-  # ====================
-  # System Packages
-  # ====================
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim
-    wget
-    ntfs3g
-    exfat
-    gcc
-    ncurses
-    gnumake
-    direnv
-    steam
-    steam-run
-    protontricks
-    # support both 32- and 64-bit applications
-    wineWowPackages.stable
-    # support 32-bit only
-    wine
-    # support 64-bit only
-    (wine.override { wineBuild = "wine64"; })
-    # wine-staging (version with experimental features)
-    wineWowPackages.staging
-    # winetricks (all versions)
-    winetricks
-    # native wayland support (unstable)
-    wineWowPackages.waylandFull
-    lutris
-    (lutris.override {
-        extraLibraries =  pkgs: [
-          # List library dependencies here
-        ];
-         extraPkgs = pkgs: [
-           # List package dependencies here
-         ];
-      })
-  ];
-
-  # ====================
-  # Games
-  # ====================
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-
-  # ====================
-  # Display
-  # ====================
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # enable sway window manager
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
-
-  # ====================
-  # Nvidia
-  # ====================
-
-  # nvidia setup
-  # Make sure opengl is enabled
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-  # Tell Xorg to use the nvidia driver
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    # Modesetting is needed for most wayland compositors
-    modesetting.enable = true;
-    # Use the open source version of the kernel module
-    # Only available on driver 515.43.04+
-    open = true;
-    # Enable the nvidia settings menu
-    nvidiaSettings = true;
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  # ====================
-  # Fonts
-  # ====================
-
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-    source-code-pro
-    roboto
-    roboto-mono
-  ]; 
-
-  # ====================
-  # Sound
-  # ====================
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = false;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # ====================
-  # Bootloader
-  # ====================
-
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # ====================
-  # Networking
-  # ====================
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -208,10 +24,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
   networking.nameservers = ["94.140.14.140" "94.140.14.141"];
-
-  # ====================
-  # System
-  # ====================
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
@@ -234,12 +46,120 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # ====================
-  # Printing
-  # ====================
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ryan = {
+    isNormalUser = true;
+    description = "ryan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      git
+      go
+      rustup
+      neovim
+      vscode
+      krita
+      discord
+      spotify      
+      firefox
+      kate
+      # latte-dock
+    ];
+  };
+
+  fonts.fonts = with pkgs; [
+      roboto
+      roboto-mono
+      source-code-pro
+      fira-code
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+      neovim
+      unzip
+      spectacle
+      wget
+      ntfs3g
+      exfat
+  ];
+
+  # ====================
+  # Nvidia
+  # ====================
+
+  # nvidia setup
+  # Make sure opengl is enabled
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  # Tell Xorg to use the nvidia driver
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    # Modesetting is needed for most wayland compositors
+    modesetting.enable = true;
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = true;
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+    forceFullCompositionPipeline = true;
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  systemd.user.services.nvidia-settings = {
+    enable = true;
+    serviceConfig = {
+      ExecStart = "${pkgs.writeScript "nvidia-settings.sh" ''
+        #!/bin/sh
+        nvidia-settings --assign CurrentMetaMode="DP-0: 2560_1440_100 +0+0 { ForceFullCompositionPipeline = On }, DP-2: 2560_1440_100 +2560+0 { ForceFullCompositionPipeline = On }"
+      ''}";
+      Restart = "always";
+      RestartSec = "5s";
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -267,4 +187,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
+
 }
