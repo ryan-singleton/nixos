449b355d90d7cb36fac995d1d61001e20469c763
ryan <ryan@northofreal.com>
2025-07-25 19:06:46 -0700

    trying neofetch

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -3,12 +3,12 @@
 { lib, config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ fastfetch fish oh-my-fish ];
+  environment.systemPackages = with pkgs; [ neofetch fish oh-my-fish ];
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       function fish_greeting
-        fastfetch
+        neofetch
       end
     '';
   };
