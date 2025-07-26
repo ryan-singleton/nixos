44ef99e21ac8c003f33679466be8566aaa72d9a9
ryan <ryan@northofreal.com>
2025-07-25 19:15:00 -0700

    Make fastfetch non automatic

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -3,13 +3,6 @@
 { lib, config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ neofetch fish oh-my-fish ];
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      function fish_greeting
-        neofetch
-      end
-    '';
-  };
+  environment.systemPackages = with pkgs; [ fastfetch fish oh-my-fish ];
+  programs.fish.enable = true;
 }
