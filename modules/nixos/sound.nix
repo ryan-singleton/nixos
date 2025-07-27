1f16a732d5a652134b9762df747d9ecce41a5423
ryan <ryan@northofreal.com>
2025-07-26 21:31:42 -0700

    No need for low latency stuff

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -6,32 +6,11 @@
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
-  # largely taking cues from this https://gitlab.com/Zaney/zaneyos/-/blob/main/modules/core/services.nix
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 256;
-        "default.clock.min-quantum" = 256;
-        "default.clock.max-quantum" = 256;
-      };
-    };
-    extraConfig.pipewire-pulse."92-low-latency" = {
-      context.modules = [{
-        name = "libpipewire-module-protocol-pulse";
-        args = {
-          pulse.min.req = "256/48000";
-          pulse.default.req = "256/48000";
-          pulse.max.req = "256/48000";
-          pulse.min.quantum = "256/48000";
-          pulse.max.quantum = "256/48000";
-        };
-      }];
-    };
   };
 }
