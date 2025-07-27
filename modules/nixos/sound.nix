042ef3f48d1b1019f88120f587cc22fe9e214469
ryan <ryan@northofreal.com>
2025-07-26 21:22:44 -0700

    If we enable the jack, don't need the kernel param

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -3,21 +3,35 @@
 { lib, config, pkgs, ... }:
 
 {
-  boot.extraModprobeConfig = ''
-    options snd_hda_intel enable=0
-  '';
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
+
+  # largely taking cues from this https://gitlab.com/Zaney/zaneyos/-/blob/main/modules/core/services.nix
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 256;
+        "default.clock.min-quantum" = 256;
+        "default.clock.max-quantum" = 256;
+      };
+    };
+    extraConfig.pipewire-pulse."92-low-latency" = {
+      context.modules = [{
+        name = "libpipewire-module-protocol-pulse";
+        args = {
+          pulse.min.req = "256/48000";
+          pulse.default.req = "256/48000";
+          pulse.max.req = "256/48000";
+          pulse.min.quantum = "256/48000";
+          pulse.max.quantum = "256/48000";
+        };
+      }];
+    };
   };
 }
