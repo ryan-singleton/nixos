35ce99d22912ecf62d948a2e66836f95b65750ff
ryan <ryan@northofreal.com>
2025-07-27 21:50:56 -0700

    yeah let's autoupdate

--- modules/nixos/maintenance.nix
+++ modules/nixos/maintenance.nix
@@ -6,15 +6,15 @@
   # Set a limit on the number of generations to include in boot
   boot.loader.systemd-boot.configurationLimit = 20;
 
-  # # Enable auto-upgrades.
-  # system.autoUpgrade = {
-  #   enable = true;
-  #   # Run daily
-  #   dates = "daily";
-  #   # Build the new config and make it the default, but don't switch yet.  This will be picked up on reboot.  This helps
-  #   # prevent issues with OpenSnitch configs not well matching the state of the system.
-  #   operation = "boot";
-  # };
+  # Enable auto-upgrades.
+  system.autoUpgrade = {
+    enable = true;
+    # Run daily
+    dates = "daily";
+    # Build the new config and make it the default, but don't switch yet.  This will be picked up on reboot.  This helps
+    # prevent issues with OpenSnitch configs not well matching the state of the system.
+    operation = "boot";
+  };
 
   # Auto GC
   nix.gc = {
