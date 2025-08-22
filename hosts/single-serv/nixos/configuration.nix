f788aeeff6fe392a8f787c46860010e048714fec
ryan <ryan@northofreal.com>
2025-08-21 18:57:04 -0700

    Main user now accepts multiple users

--- hosts/single-serv/nixos/configuration.nix
+++ hosts/single-serv/nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, outputs, ... }:
 
 {
   imports = [
@@ -37,7 +37,7 @@
 
   # Set up the user
   main-user.enable = true;
-  main-user.userName = "ryan";
+  main-user.userNames = [ "ryan" "uriah" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -46,5 +46,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
--- modules/nixos/main-user.nix
+++ modules/nixos/main-user.nix
@@ -1,25 +1,28 @@
 # main-user.nix
-# a bit overdone, but used as a reference for setting up options
-
 { lib, config, pkgs, ... }:
+let
+  cfg = config.main-user;
 
-let cfg = config.main-user;
+  userConfigs = lib.listToAttrs (map (userName: {
+    name = userName;
+    value = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" ];
+    };
+  }) cfg.userNames);
+
 in {
   options.main-user = {
     enable = lib.mkEnableOption "enable main user module";
-    userName = lib.mkOption {
-      default = "mainuser";
+
+    userNames = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "mainuser" ];
       description = ''
-        username
+        List of usernames to create as main users
       '';
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    users.users.${config.main-user.userName} = {
-      isNormalUser = true;
-      extraGroups = [ "networkmanager" "wheel" ];
-    };
-  };
+  config = lib.mkIf cfg.enable { users.users = userConfigs; };
 }
-
