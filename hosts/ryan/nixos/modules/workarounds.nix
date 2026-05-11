822307b885ed384ea19f88c14cfe514aa989afa9
ryan-singleton <ryan@northofreal.com>
2026-05-10 20:35:52 -0700

    ugg, add the workaround back

--- hosts/ryan/nixos/modules/workarounds.nix
+++ hosts/ryan/nixos/modules/workarounds.nix
@@ -12,14 +12,11 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      # I've been stuck in one version of NixOS for a very long time because of an openldap
-      # test issue that is inconsequential, so far as I can tell. This is bad form and the org
-      # behind the distro needs to get this resolved. Causing a great many concerns about
-      # sticking to NixOS. It needs to be reliable if I'm going to put this much work into it!
-      # Ignoring it and moving on.
-      # https://github.com/NixOS/nixpkgs/issues/372569
-      openldap = prev.openldap.overrideAttrs (old: {
+      openldap = prev.openldap.overrideAttrs (_: {
         doCheck = false;
+      });
+      fwupd = prev.fwupd.overrideAttrs (old: {
+        mesonFlags = (old.mesonFlags or [ ]) ++ [ "-Ddocs=disabled" ];
       });
     })
   ];
