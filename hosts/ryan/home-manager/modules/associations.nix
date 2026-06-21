959cdd7494e4b69b3e94c82c3b13537cd94eb566
ryan-singleton <ryan@northofreal.com>
2026-06-21 16:48:45 -0700

    set zen browser as default for ryan

--- hosts/ryan/home-manager/modules/associations.nix
+++ hosts/ryan/home-manager/modules/associations.nix
@@ -1,13 +1,22 @@
 # associations.nix
 # set default applications for links, filetypes, and mime types in general
-{ lib, config, pkgs, ... }: {
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
   xdg.mimeApps = {
     enable = true;
-    associations.added = { "image/svg+xml" = "inkscape.desktop"; };
+    associations.added = {
+      "image/svg+xml" = "inkscape.desktop";
+    };
     defaultApplications = {
-      "text/html" = "firefox.desktop";
-      "x-scheme-handler/http" = "firefox.desktop";
-      "x-scheme-handler/https" = "firefox.desktop";
+      "text/html" = "zen-beta.desktop";
+      "x-scheme-handler/http" = "zen-beta.desktop";
+      "x-scheme-handler/https" = "zen-beta.desktop";
       "image/svg+xml" = "inkscape.desktop";
     };
   };
